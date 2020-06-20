598ddaa8472d28fe109d7a32b5a5ee301e948772
jasper <cool.daniel1234@gmail.com>
2020-06-20 16:45:09 +0800

    update

---
+++ URLImage.swift
@@ -0,0 +1,33 @@
+//
+//  URLImage.swift
+//  Ding
+//
+//  Created by User02 on 2020/6/19.
+//  Copyright © 2020 sun. All rights reserved.
+//
+
+import SwiftUI
+import Foundation
+
+struct URLImage: View {
+    var url: URL
+    @State private var image = Image(systemName: "photo")
+    @State private var downloadimgfinish = false
+    
+    func download() {
+        if let data = try? Data(contentsOf: url), let uiImage = UIImage(data: data) {
+            self.image = Image(uiImage: uiImage)
+            self.downloadimgfinish = true
+        }
+    }
+    
+    var body: some View {
+        image
+        .resizable()
+            .onAppear {
+                if self.downloadimgfinish == false {
+                    self.download()
+                }
+        }
+    }
+}
